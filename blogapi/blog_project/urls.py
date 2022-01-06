238e9c7d94324778fb2081db7722d66244fec55e
Ahmedgadein <ahmed.g.obied@gmail.com>
2022-01-06 13:09:21 +0200

    user authentication

--- blogapi/blog_project/urls.py
+++ blogapi/blog_project/urls.py
@@ -18,7 +18,9 @@ from django.urls import path
 from django.urls.conf import include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/', include('posts.urls')),
-    path('api-auth', include('rest_framework.urls'))
+    path("admin/", admin.site.urls),
+    path("api/v1/", include("posts.urls")),
+    path("api-auth", include("rest_framework.urls")),
+    path("api/v1/rest-auth/", include("rest_auth.urls")),
+    path("api/v1/rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
