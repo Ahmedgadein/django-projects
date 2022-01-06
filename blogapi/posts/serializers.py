fb5be449ae99024db9865e287d51aac6cb0037d0
Ahmedgadein <ahmed.g.obied@gmail.com>
2022-01-06 15:23:30 +0200

    viewsets and routers

--- blogapi/posts/serializers.py
+++ blogapi/posts/serializers.py
@@ -1,7 +1,17 @@
+from django.db import models
+from django.db.models import fields
 from rest_framework import serializers
 from .models import Post
+from django.contrib.auth import get_user_model
+
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ('title', 'body', 'id', 'author', 'created_at')
+        fields = ("title", "body", "id", "author", "created_at")
+
+
+class UserSerizalizer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ("id","username",)
--- blogapi/posts/views.py
+++ blogapi/posts/views.py
@@ -1,14 +1,36 @@
-from rest_framework import generics, permissions
+from rest_framework import viewsets
 from .models import Post
-from .serializers import PostSerializer
+from .serializers import PostSerializer, UserSerizalizer
 from .permissions import IsAuthorOrReadOnly
+from django.contrib.auth import get_user_model
 
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+
+class PostViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerizalizer
 
-class PostList(generics.ListCreateAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+
+# class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+#     permission_classes = (IsAuthorOrReadOnly,)
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+
+
+# class PostList(generics.ListCreateAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+
+
+# class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = get_user_model().objects.all()
+#     serializer_class = UserSerizalizer
+
+
+# class UserList(generics.ListCreateAPIView):
+#     queryset = get_user_model().objects.all()
+#     serializer_class = UserSerizalizer
