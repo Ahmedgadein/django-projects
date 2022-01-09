108dae9e12132c6043c0ec59c2bbf1f560a47db1
Ahmedgadein <ahmed.g.obied@gmail.com>
2022-01-10 01:32:42 +0200

    schema and docs

--- blogapi/blog_project/urls.py
+++ blogapi/blog_project/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
+
+schema_view = get_schema_view(title="Blog API")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -23,4 +27,6 @@ urlpatterns = [
     path("api-auth", include("rest_framework.urls")),
     path("api/v1/rest-auth/", include("rest_auth.urls")),
     path("api/v1/rest-auth/registration/", include("rest_auth.registration.urls")),
+    path("schema/", schema_view),
+    path("docs/", include_docs_urls(title="Blog API")),
 ]
